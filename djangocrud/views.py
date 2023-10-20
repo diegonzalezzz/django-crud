ee6fc6cf6304b488d2ec4c9422d960f74f1f126b
diegonzalezzz <diegocornejo890@gmail.com>
2023-10-19 22:00:43 -0300

    first commit

---
+++ djangocrud/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+def helloworld(request):
+    
+    return render(request, 'signup.html',{
+        'form' : UserCreationForm,
+    })
